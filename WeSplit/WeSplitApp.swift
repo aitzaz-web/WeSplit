67cd1d75b076b1b90df51eda8d55cabf4c725228
Aitzaz Munir Shaikh <ams845@cornell.edugit config --global user.email ams845@cornell.edu>
2024-06-21 16:14:39 +0500

    Initial Commit

---
+++ WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Aitzaz Munir on 21/06/2024.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
